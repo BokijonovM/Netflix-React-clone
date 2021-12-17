bde9d4e2f65c039b5f784442701259ae65dd3114
DonAzi <savaSh-00@mail.ru>
2021-12-17 16:40:00 +0000

    fixing folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyFooter from "./components/MyFooter";
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
