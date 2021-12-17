fcd7e6c45dc1cba2219074f2a72fa165f5e2684b
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-17 16:33:00 +0100

    section part

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './component/Navbar';
+import TvShows from './components/TvShows'
+import Section from './components/Section'
 
 function App() {
   return (
@@ -8,7 +10,12 @@ function App() {
       <NavBar />
       <header className="App-header">
         <TvShows />
+        <Section title="Harry Potter"></Section>
+        <Section title="Lord of the Rings"></Section>
+        <Section title="Horror"></Section>
+        <Section title="Action"></Section>
       </header>
+
     </div>
   );
 }
--- react-netflix/src/component/Navbar.jsx
+++ react-netflix/src/component/Navbar.jsx
@@ -1,75 +1,73 @@
-import logo from '../logo.svg';
-
 import React, { Component } from 'react';
-import {Navbar, Nav, Form, FormControl, Button, Image} from 'react-bootstrap'
+import { Navbar, Nav, Form, FormControl, Button, Image } from 'react-bootstrap'
 // import CardSearch from '../component/CardSearch'
- import brand from '../brand.png';
- import user from '../user.png';
+import brand from '../brand.png';
+import user from '../user.png';
 
- // ...later
+// ...later
 
 class NavBar extends React.Component {
     state = {
-        searchQuery:'',
-        moviesArr:[]
+        searchQuery: '',
+        moviesArr: []
     }
 
-   performSearch = async(e) =>{
+    performSearch = async (e) => {
         e.preventDefault()
         try {
-            let response = await fetch("http://www.omdbapi.com/?apikey=82ebb69a&s=" + this.state.searchQuery.toLowerCase(),{
-                method:"GET"
+            let response = await fetch("http://www.omdbapi.com/?apikey=82ebb69a&s=" + this.state.searchQuery.toLowerCase(), {
+                method: "GET"
             })
             let data = await response.json()
-            this.setState({moviesArr:data})
+            this.setState({ moviesArr: data })
             console.log(data)
-           
-            
+
+
         } catch (error) {
             console.log(error)
         }
     }
 
-    render() { 
+    render() {
         return (
-        <div>
-                                <Navbar  bg="dark" variant="dark" expand="lg" fixed>
-                                        <Navbar.Brand href=""><Image src={brand} style={{width:"100px"}} fluid /> </Navbar.Brand>
-                                        <Navbar.Toggle aria-controls="navbarScroll" />
-                                        <Navbar.Collapse id="navbarScroll">
-                                            <Nav
-                                            className="mr-auto my-2 my-lg-0"
-                                            style={{ maxHeight: '100px' }}
-                                            navbarScroll
-                                            >
-                                                
-                                            <Nav.Link href="#home">Home</Nav.Link>
-                                            <Nav.Link href="#features">TV Show</Nav.Link>
-                                            <Nav.Link href="#movies">Movies</Nav.Link>
-                                            <Nav.Link href="#recentlyAdded">Recently Added</Nav.Link>
-                                            <Nav.Link href="#myList">My List</Nav.Link>
-                                            </Nav>
+            <div>
+                <Navbar bg="dark" variant="dark" expand="lg" fixed>
+                    <Navbar.Brand href=""><Image src={brand} style={{ width: "100px" }} fluid /> </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="mr-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
 
-                                            <Nav className="d-flex align-items-center">
-                                        <Form inline className="text-center">
-                                                <FormControl type="text" placeholder="Search" className="mr-sm-2" 
-                                                value = {this.state.searchQuery}
-                                                onChange={(e) => this.setState({searchQuery:e.target.value}) } />
-                                                    <Nav.Link href="#features" onClick={this.performSearch}><i class="bi bi-search"></i></Nav.Link>
-                                                   <div className="d-flex align-items-center">
-                                                   <Nav.Link href="#features">KIDS</Nav.Link>
-                                                    <Nav.Link href="#bellIcon"><i  className = " text-white bi bi-bell-fill"></i> </Nav.Link>
-                                                    <Nav.Link href="#pricing"><Image src={user} width="40px"/></Nav.Link>
-                                                   </div>
-                                         </Form>
-                                                </Nav>
-                                    </Navbar.Collapse>
-                              </Navbar>
-                              
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">TV Show</Nav.Link>
+                            <Nav.Link href="#movies">Movies</Nav.Link>
+                            <Nav.Link href="#recentlyAdded">Recently Added</Nav.Link>
+                            <Nav.Link href="#myList">My List</Nav.Link>
+                        </Nav>
+
+                        <Nav className="d-flex align-items-center">
+                            <Form inline className="text-center">
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2"
+                                    value={this.state.searchQuery}
+                                    onChange={(e) => this.setState({ searchQuery: e.target.value })} />
+                                <Nav.Link href="#features" onClick={this.performSearch}><i class="bi bi-search"></i></Nav.Link>
+                                <div className="d-flex align-items-center">
+                                    <Nav.Link href="#features">KIDS</Nav.Link>
+                                    <Nav.Link href="#bellIcon"><i className=" text-white bi bi-bell-fill"></i> </Nav.Link>
+                                    <Nav.Link href="#pricing"><Image src={user} width="40px" /></Nav.Link>
+                                </div>
+                            </Form>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+
                 {/* <CardSearch movieList={this.state.moviesArr}/> */}
-        </div>
-)    
+            </div>
+        )
+    }
 }
-}
- 
+
 export default NavBar;
