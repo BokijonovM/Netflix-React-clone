68ca3c709be01efa0c48ea0b9c6c940fdc958b84
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-17 09:58:27 +0000

    image added

--- react-netflix/src/component/Navbar.jsx
+++ react-netflix/src/component/Navbar.jsx
@@ -10,14 +10,23 @@ class NavBar extends React.Component {
              <Navbar className='MyNavBar' bg="dark" variant="dark">
                     <Navbar.Brand href="#home"> <img src='/react-netflix/public/assets/netflix_logo.png' alt="netflix logo"/> </Navbar.Brand>
                     <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">TV Show</Nav.Link>
+                        <Nav.Link href="#pricing">Movies</Nav.Link>
+                        <Nav.Link href="#pricing">Recently Added</Nav.Link>
+                        <Nav.Link href="#pricing">My List</Nav.Link>
                     </Nav>
                     <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-info">Search</Button>
                     </Form>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">TV Show</Nav.Link>
+                        <Nav.Link href="#pricing">Movies</Nav.Link>
+                        <Nav.Link href="#pricing">Recently Added</Nav.Link>
+                        <Nav.Link href="#pricing">My List</Nav.Link>
+                    </Nav>
                 </Navbar>
                
         </div>
