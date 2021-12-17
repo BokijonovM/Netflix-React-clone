2ca902b73c207622c090a5d2e3aad9fea61e7a2a
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-17 13:21:54 +0100

    updated

--- react-netflix/src/components/DisplayMovies.jsx
+++ react-netflix/src/components/DisplayMovies.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
 import CardSearch from './CardSearch'
 
 class DisplayMovies extends React.Component {
-    render() { 
+    render() {
         return (<>
-              {this.props.movieList.map(movie => <CardSearch key={movie.imdbID} movie={movie}></CardSearch>)}
+            {this.props.movieList.map(movie => <CardSearch key={movie.imdbID} movie={movie}></CardSearch>)}
         </>);
     }
 }
- 
+
 export default DisplayMovies;
