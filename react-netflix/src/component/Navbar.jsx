9345b7c37b0ed380c66b4dff2e6f52bacac56771
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-17 11:11:50 +0000

    search query add

--- react-netflix/src/component/Navbar.jsx
+++ react-netflix/src/component/Navbar.jsx
@@ -4,30 +4,56 @@ import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap'
 
 
 class NavBar extends React.Component {
+    state = {
+        searchQuery:''
+    }
+
+   performSearch = async function(e){
+        e.preventDefault()
+        try {
+            let response = await fetch("http://www.omdbapi.com/?apikey=82ebb69a&s=" + this.state.searchQuery,{
+                method:"GET"
+            })
+            let data = await response.json()
+            console.log(data)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     render() { 
         return (
         <div>
-             <Navbar className='MyNavBar' bg="dark" variant="dark">
-                    <Navbar.Brand href="#home"> <img src='/react-netflix/public/assets/netflix_logo.png' alt="netflix logo"/> </Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">TV Show</Nav.Link>
-                        <Nav.Link href="#pricing">Movies</Nav.Link>
-                        <Nav.Link href="#pricing">Recently Added</Nav.Link>
-                        <Nav.Link href="#pricing">My List</Nav.Link>
-                    </Nav>
-                    <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                    </Form>
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">TV Show</Nav.Link>
-                        <Nav.Link href="#pricing">Movies</Nav.Link>
-                        <Nav.Link href="#pricing">Recently Added</Nav.Link>
-                        <Nav.Link href="#pricing">My List</Nav.Link>
-                    </Nav>
-                </Navbar>
+                                <Navbar  bg="dark" variant="dark" expand="lg">
+                                        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                                        <Navbar.Toggle aria-controls="navbarScroll" />
+                                        <Navbar.Collapse id="navbarScroll">
+                                            <Nav
+                                            className="mr-auto my-2 my-lg-0"
+                                            style={{ maxHeight: '100px' }}
+                                            navbarScroll
+                                            >
+                                            <Nav.Link href="#home">Home</Nav.Link>
+                                            <Nav.Link href="#features">TV Show</Nav.Link>
+                                            <Nav.Link href="#movies">Movies</Nav.Link>
+                                            <Nav.Link href="#recentlyAdded">Recently Added</Nav.Link>
+                                            <Nav.Link href="#myList">My List</Nav.Link>
+                                            </Nav>
+
+                                        <Form inline>
+                                                <FormControl type="text" placeholder="Search" className="mr-sm-2" 
+                                                value = {this.state.searchQuery}
+                                                onChange={(e) => this.setState({searchQuery:e.target.value}) } />
+                                                    <Nav.Link href="#features" onClick={this.performSearch}><i class="bi bi-search"></i></Nav.Link>
+                                                <Nav className="mr-auto">
+                                                    <Nav.Link href="#features">KIDS</Nav.Link>
+                                                    <Nav.Link href="#bellIcon"><i  className = " text-white bi bi-bell-fill"></i> </Nav.Link>
+                                                    <Nav.Link href="#pricing"><img src='/react-netflix/public/assets/kids_icon.png'/></Nav.Link>
+                                                </Nav>
+                                         </Form>
+                                    </Navbar.Collapse>
+                              </Navbar>
                
         </div>
 )    
