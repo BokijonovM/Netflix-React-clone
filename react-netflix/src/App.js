17c04633e4d3e1db5693f54d5e4f0fbf679c8862
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-17 15:31:53 +0000

    tvshow page fixed

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './component/Navbar';
-
+import TvShows from './component/TvShows.jsx'
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark">
       <NavBar/>
-      <header className="App-header">
+      
         <TvShows />
-      </header>
+        
+    
     </div>
   );
 }
