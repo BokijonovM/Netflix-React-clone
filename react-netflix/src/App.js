7847d62d35ef7d71316ced079fa92052c0f0523a
DonAzi <savaSh-00@mail.ru>
2021-12-17 15:43:20 +0000

    Footer

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MyFooter />
     </div>
   );
 }
