f63d5c50202deb107cced00e06824b6e0ce1eb64
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-19 19:33:52 +0000

    comment modal displayed

--- src/components/CardSearch.js
+++ src/components/CardSearch.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import { Card, } from 'react-bootstrap'
+import { Component, useState } from 'react'
+import { Card, Button} from 'react-bootstrap'
+import  Modal from 'react-bootstrap/Modal'
 import './style.css'
 
 
@@ -8,7 +9,8 @@ export default class CardSearch extends Component {
         showPrice: null,
         btnColor: 'primary',
         cardClass: "px-1 py-4 m-0 border-0 w-100 bg-transparent",
-        isClicked: false
+        isClicked: false,
+        showComments:false
     }
 
     commentsDisplay = () => {
@@ -27,19 +29,88 @@ export default class CardSearch extends Component {
     //         padding: "0px"
     //     })
     // }
-
+    setShowComments = (show) =>{
+        this.setState({showComments:show})
+    }
+    
+    showComments = () => this.setShowComments(true)
+    hideComments = () => this.setShowComments(false)
 
     render() {
         return (
             <Card className={this.state.cardClass} style={{maxWidth:"18rem"}}>
-                <Card.Img onClick={(e) => this.commentsDisplay()} className="img-poster align-self-center" variant="top" src={this.props.movie.Poster} />
+                <Card.Img onClick={this.showComments} className="img-poster align-self-center" variant="top" src={this.props.movie.Poster} />
                 <div className="position-relative">
                     <Card.Body className="d-flex flex-column pb-0 pt-2 px-0 justify-content-between w-100 position-absolute">
                         {this.state.isClicked && <Card.Title className="mb-2 px-2"> <h6>{this.props.movie.Title}</h6> <h6>{this.props.movie.imdbID}</h6></Card.Title>}
                     </Card.Body>
                 </div>
-                
+                        {this.state.showComments && (
+                        <Modal show={true} onHide={this.hideComments}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Comments List</Modal.Title>
+                                </Modal.Header>
+
+                                <Modal.Body>
+                                    <p>Modal body text goes here.</p>
+                                </Modal.Body>
+
+                                <Modal.Footer>
+                                    <Button variant="secondary">Close</Button>
+                                </Modal.Footer>
+                        </Modal>)}
             </Card>
         )
     }
 }
+
+   
+// import { Component, useState } from "react"
+// import CommentsArea from "./CommentArea"
+// import Modal from "react-bootstrap/Modal"
+// import "../App.css"
+
+// class MovieCard extends Component {
+//   state = {
+//     showComments: false,
+//   }
+
+//   setShowComments = (show) => {
+//     this.setState({
+//       showComments: show,
+//     })
+//   }
+
+//   showComments = () => this.setShowComments(true)
+//   hideComments = () => this.setShowComments(false)
+
+//   render() {
+//     return (
+//       <div>
+//         <img
+//           className="img-fluid w-100 movie-img"
+//           src={this.props.movie.Poster}
+//           alt={this.props.movie.Title}
+//           onClick={this.showComments}
+//         />
+//         {this.state.showComments && (
+//           <Modal show={true} onHide={this.hideComments}>
+//             <Modal.Header closeButton>
+//               <Modal.Title>Modal title</Modal.Title>
+//             </Modal.Header>
+
+//             <Modal.Body>
+//               <CommentsArea id={this.props.movie.imdbID} />
+//             </Modal.Body>
+
+//             <Modal.Footer>
+//               {/* <AddComment fetchComments={this.reload} id={this.props.asin} /> */}
+//             </Modal.Footer>
+//           </Modal>
+//         )}
+//       </div>
+//     )
+//   }
+// }
+
+// export default MovieCard
