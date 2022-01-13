98001080f3e57a8c237edd3cb07a8efc73dddcd8
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-13 15:27:46 +0100

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,28 +1,132 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, Button, Col, Row} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 function Register() {
+
+    const [name, setName] = useState('')
+    const [ surname, setSurname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [birth, setBirth] = useState('')
+    const [city, setCity] = useState('')
+    const [street, setStreet] = useState('')
+    const [postal, setPostal] = useState('')
+
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    const handleName = (e) => {
+        setName(e.target.value);
+        setSubmitted(false);
+    };
+    const handleSurname = (e) => {
+        setSurname(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        setSubmitted(false);
+    };    
+ 
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handleCity = (e) => {
+        setCity(e.target.value);
+        setSubmitted(false);
+    };    
+ 
+
+    const handleStreet = (e) => {
+        setStreet(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handlePostal = (e) => {
+        setPostal(e.target.value);
+        setSubmitted(false);
+    };    
+ 
+
+    const handleBirth = (e) => {
+        setBirth(e.target.value);
+        setSubmitted(false);
+    };
+
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (name === '' || email === '' || password === '') {
+        setError(true);
+        } else {
+        setSubmitted(true);
+        setError(false);
+        }
+    };
+
+
+
+    const successMessage = () => {
+        return (
+        <div
+            className="success"
+            style={{
+            display: submitted ? '' : 'none',
+            }}>
+            <h1 className='d-flex justify-content-center'>User {name} successfully registered!!</h1>
+        </div>
+        );
+    };
+ 
+  // Showing error message if error is true
+    const errorMessage = () => {
+        return (
+        <div
+            className="error"
+            style={{
+            display: error ? '' : 'none',
+            }}>
+            <h1 className='d-flex justify-content-center' >Please enter all the fields</h1>
+        </div>
+        );
+    };
+
+
+
     return (
         <div>
             <h2 className='pl-5 mt-4 mb-4'>Welcome to Login page</h2>
+            <div className="messages">
+                {errorMessage()}
+                {successMessage()}
+            </div>
+
             <Form className='d-flex justify-content-center'>
                 <Row className='container my-5 register-row-cont'  >
                     <Col>
                         <div className='register-div d-flex justify-content-center'>
                             <Form.Group  controlId="formBasicName">
                             <Form.Label >First Name</Form.Label>
-                            <Form.Control required type="text" placeholder="Enter name" />
+                            <Form.Control onChange={handleName} value={name} required type="text" placeholder="Enter name" />
                         </Form.Group>
                         <Form.Group controlId="formBasicSurname">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control required type="text" placeholder="Enter surname" />
+                            <Form.Control onChange={handleSurname} value={surname} required type="text" placeholder="Enter surname" />
                         </Form.Group>
                         </div>
 
                        <div className='register-div d-flex justify-content-center'>
                             <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control required type="email" placeholder="Enter email" />
+                            <Form.Control onChange={handleEmail} value={email} required type="email" placeholder="Enter email" />
                             {/* <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                             </Form.Text> */}
@@ -30,7 +134,7 @@ function Register() {
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control required type="password" placeholder="Password" />
+                            <Form.Control onChange={handlePassword} value={password} required type="password" placeholder="Password" />
                         </Form.Group>
                        </div>
 
@@ -38,29 +142,34 @@ function Register() {
                          <div className='register-div d-flex justify-content-center'>
                          <Form.Group controlId="formBasicCity">
                             <Form.Label>City address</Form.Label>
-                            <Form.Control required type="text" placeholder="Enter your city address" />
+                            <Form.Control onChange={handleCity} value={city} required type="text" placeholder="Enter your city address" />
                         </Form.Group>
                         <Form.Group controlId="formBasicStreet">
                             <Form.Label>Street address</Form.Label>
-                            <Form.Control required type="text" placeholder="Enter your street address" />
+                            <Form.Control onChange={handleStreet} value={street} required type="text" placeholder="Enter your street address" />
                         </Form.Group>
                      </div>
 
                         <div className='register-div d-flex justify-content-center'>
                             <Form.Group controlId="formBasicPostal">
                                 <Form.Label>Postal code</Form.Label>
-                                <Form.Control required type="text" placeholder="Enter postal code" />
+                                <Form.Control onChange={handlePostal} value={postal} required type="number" placeholder="Enter postal code" />
                             </Form.Group>
                              <Form.Group className='' controlId="formBasicDate">
                                 <Form.Label>Date of birth</Form.Label>
-                                <Form.Control required type="date" placeholder="Enter your street address" />
+                                <Form.Control onChange={handleBirth} value={birth} required type="date" placeholder="Enter your street address" />
                         </Form.Group>
                         </div>
                         
-                             
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
+                        <div className='register-div d-flex justify-content-center'>
+                            
+                                <Button onClick={handleSubmit} variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                        </div>
+                        
+                           
+                        
 
                         
                         
