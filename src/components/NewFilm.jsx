86a75eb8e49374a2602def7b786c37536d1c012e
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-04 15:03:50 +0100

    new from group added

--- src/components/NewFilm.jsx
+++ src/components/NewFilm.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 function NewFilm() {
-  return <div></div>;
+  return (
+    <div>
+      <Form className="add-new-form">
+        <div className="d-flex">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </div>
+        <div className="d-flex">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </div>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default NewFilm;
