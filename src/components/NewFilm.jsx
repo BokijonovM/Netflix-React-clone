2676af0b5e90a0b0bf160c172369798ce1157c40
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-04 15:05:39 +0100

    as

--- src/components/NewFilm.jsx
+++ src/components/NewFilm.jsx
@@ -9,9 +9,6 @@ function NewFilm() {
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
@@ -23,9 +20,6 @@ function NewFilm() {
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
